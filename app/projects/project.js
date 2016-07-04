58fc1adead295d7980e4bcf73d13a07807fe0910
informatica <gomezcarlose@gmail.com>
2016-07-04 15:25:18 -0400

    FIX service

---
+++ app/projects/project.js
@@ -0,0 +1,9 @@
+"use strict";
+var Project = (function () {
+    function Project() {
+        // code...
+    }
+    return Project;
+}());
+exports.Project = Project;
+//# sourceMappingURL=project.js.map
